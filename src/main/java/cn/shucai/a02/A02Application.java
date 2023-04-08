cc14c6904405567863d53eae0102ce353f184352
shucaijingling <A421750169@126.com>
2023-04-09 02:28:16 +0800

    BeanFactory -- BeanDefinition --> webServer

--- src/main/java/cn/shucai/a02/A02Application.java
+++ src/main/java/cn/shucai/a02/A02Application.java
@@ -2,11 +2,17 @@ package cn.shucai.a02;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.Controller;
 
 public class A02Application {
 
@@ -15,20 +21,22 @@ public class A02Application {
     public static void main(String[] args) {
 //        testClassPathXmlApplicationContext();
 //        testFileSystemXmlApplicationContext();
+//        testAnnotationConfigApplicationContext();
+        testAnnotationConfigServletWebServerApplicationContext();
 
 
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        System.out.println("读取之前： ");
-        for (String name : beanFactory.getBeanDefinitionNames()) {
-            System.out.println(name);
-        }
-
-        System.out.println("读取之后:");
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions(new ClassPathResource("b01.xml"));
-        for (String name : beanFactory.getBeanDefinitionNames()) {
-            System.out.println(name);
-        }
+//        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+//        System.out.println("读取之前： ");
+//        for (String name : beanFactory.getBeanDefinitionNames()) {
+//            System.out.println(name);
+//        }
+//
+//        System.out.println("读取之后:");
+//        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+//        reader.loadBeanDefinitions(new ClassPathResource("b01.xml"));
+//        for (String name : beanFactory.getBeanDefinitionNames()) {
+//            System.out.println(name);
+//        }
 
     }
 
@@ -57,11 +65,73 @@ public class A02Application {
         System.out.println(context.getBean(Bean2.class).getBean1());
     }
 
-    static class Bean1{
+    //基于 java 配置类来创建
+    private static void testAnnotationConfigApplicationContext() {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+
+        System.out.println(context.getBean(Bean2.class).getBean1());
+    }
+
+    //基于java 配置类来创建，用于web环境
+    private static void testAnnotationConfigServletWebServerApplicationContext() {
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+    }
+
+    @Configuration
+    static class WebConfig {
+        //1. 创建tomcat webServer 服务器
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory() {
+            return new TomcatServletWebServerFactory();
+        }
+        //2. 创建dispatcherServlet
+        @Bean
+        public DispatcherServlet dispatcherServlet() {
+            return new DispatcherServlet();
+        }
+
+        //3.将dispatcherServlet注册到tomcat
+        @Bean
+        public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet) {
+            return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        }
+
+        //4. 处理请求
+        @Bean("/hello")
+        public Controller controller1() {
+            return (request, response) -> {
+                response.getWriter().println("hello");
+                return null;
+            };
+        }
+    }
+
+    @Configuration
+    static class Config {
+        @Bean
+        public Bean1 bean1() {
+            return new Bean1();
+        }
+
+        @Bean
+        public Bean2 bean2(Bean1 bean1) {
+            Bean2 bean2 = new Bean2();
+            bean2.setBean1(bean1);
+            return bean2;
+        }
+    }
+
+    static class Bean1 {
 
     }
 
-    static class Bean2{
+    static class Bean2 {
         private Bean1 bean1;
 
         public void setBean1(Bean1 bean1) {
