75445567d363d797a0cf0ff1b94744fc7e48b1cd
shucaijingling <A421750169@126.com>
2023-04-11 23:52:17 +0800

    bean生命周期 -- 5

--- src/main/java/cn/shucai/a04/DigInAutowired.java
+++ src/main/java/cn/shucai/a04/DigInAutowired.java
@@ -3,10 +3,13 @@ package cn.shucai.a04;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.annotation.InjectionMetadata;
+import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
+import org.springframework.core.MethodParameter;
 import org.springframework.core.env.StandardEnvironment;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 //AutowiredAnnotationBeanPostProcessor
@@ -37,5 +40,22 @@ public class DigInAutowired {
         //2. 调用InjectionMetadata进行依赖注入， 注入按照类型查找值
         metadata.inject(bean1, "bean1", null);
         System.out.println(bean1);
+
+        //3. 如何按类型查找
+        Field bean3 = Bean1.class.getDeclaredField("bean3");
+        DependencyDescriptor dd1 = new DependencyDescriptor(bean3, false);
+        Object o = beanFactory.doResolveDependency(dd1, null, null, null);
+        System.out.println(o);
+
+        Method setBean2 = Bean1.class.getDeclaredMethod("setBean2", Bean2.class);
+        DependencyDescriptor dd2 = new DependencyDescriptor(new MethodParameter(setBean2, 0), false);
+        Object o1 = beanFactory.doResolveDependency(dd2, null, null, null);
+        System.out.println(o1);
+
+        Method setHome = Bean1.class.getDeclaredMethod("setHome", String.class);
+        DependencyDescriptor dd3 = new DependencyDescriptor(new MethodParameter(setHome, 0), true);
+        Object o2 = beanFactory.doResolveDependency(dd3, null, null, null);
+        System.out.println(o2);
+
     }
 }
